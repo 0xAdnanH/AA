52c473e2fc5b2364b87c76ede770d7230bbd4ff9
0xAdnanH <0xAdnanH@gmail.com>
2023-07-25 19:00:19 +0300

    modified:   test/contract.test.js

--- test/contract.test.js
+++ test/contract.test.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-
+const bytecode =
+  require("../artifacts/contracts/account.sol/account.json").bytecode;
 let owner;
 let contract;
 let addr1;
@@ -9,8 +10,6 @@ let testContract;
 before(async () => {
   [owner, addr1, addr2] = await ethers.getSigners();
   const contractFacory = await ethers.getContractFactory("account");
-  const testFactory = await ethers.getContractFactory("testContract");
-  testContract = await testFactory.deploy();
 
   contract = await contractFacory.connect(owner).deploy();
 });
@@ -30,8 +29,7 @@ describe("execute function test", async () => {
   it("testing operationtype 1", async () => {
     const operationType = 1;
     const valueToSend = 0;
-    const dataToSend =
-      "0x6080604052348015600f57600080fd5b50603f80601d6000396000f3fe6080604052600080fdfea2646970667358221220fb4361c8e2bc9cc551ae4994b235565660d2eba335297a955d1921884435cc8e64736f6c63430008130033";
+    const dataToSend = bytecode;
     const target = addr1;
     await contract
       .connect(owner)
